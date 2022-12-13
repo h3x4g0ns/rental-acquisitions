5d00e4628ac661e602a8efc785a25cbae4ecb9f4
h3x4g0ns <agrawalekansh02@gmail.com>
2022-12-13 02:08:30 -0800

    added react src code

---
+++ rental-dashboard/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import "@tremor/react/dist/esm/tremor.css";
+import "./index.css";
+import { Flex, Block, BadgeDelta, Card, Text, Metric, ProgressBar} from "@tremor/react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <Card maxWidth="max-w-lg">
+          <Flex alignItems="items-start">
+            <Block>
+              <Text>Sales</Text>
+              <Metric>$ 20,699</Metric>
+            </Block>
+            <BadgeDelta deltaType="moderateDecrease" text="13.2%" />
+          </Flex>
+          <Flex marginTop="mt-4">
+            <Text truncate={true}>50% ($ 110,250)</Text>
+            <Text> $ 220,500 </Text>
+          </Flex>
+          <ProgressBar percentageValue={50} marginTop="mt-2" />
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default App;
