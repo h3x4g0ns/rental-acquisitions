2735b86a6c93015fa4b32c28decb48c4f83803bc
h3x4g0ns <agrawalekansh02@gmail.com>
2022-12-21 15:48:11 -0800

    added loading comp

---
+++ src/lib/Loading.svelte
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<div class="flex justify-center items-center mt-4">
+  <div
+    class="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-tr from-indigo-500 to-pink-500 animate-spin"
+  >
+    <div class="h-9 w-9 rounded-full bg-gray-200" />
+  </div>
+</div>
