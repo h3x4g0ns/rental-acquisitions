fa43ecab8f00cc97a76c4f9982694bc36850bf3b
h3x4g0ns <agrawalekansh02@gmail.com>
2022-12-21 15:48:00 -0800

    added loading comp

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -56,7 +56,6 @@
       <div>
         <label for="email">Email</label>
         <input
-          type="email"
           class="w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
           id="email"
           placeholder="Your Email"
