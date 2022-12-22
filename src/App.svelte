b57032e86536c8693be9ff0c4e3114d45dab9574
h3x4g0ns <agrawalekansh02@gmail.com>
2022-12-22 15:34:03 -0800

    mods for tabs in navbar

--- src/App.svelte
+++ src/App.svelte
@@ -9,35 +9,61 @@
     NavHamburger,
     Button,
   } from "flowbite-svelte";
+
+  let menu = -1;
 </script>
 
-<div class="h-screen flex bg-gray-bg1">
+<div class="h-screen bg-gray-bg1">
   <Login />
   {#if $currentUser}
-    <Navbar let:hidden let:toggle>
-      <NavBrand href="/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          class="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
-        <span
-          class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+    <div class="w-full">
+      <Navbar let:hidden let:toggle>
+        <NavBrand
+          href="/"
+          on:click={(event) => {
+            event.preventDefault();
+            menu = 0;
+          }}
         >
-          Rental Acquisitions
-        </span>
-      </NavBrand>
-      <div class="flex md:order-2">
-        <Button size="sm" on:click={logOut}>Logout</Button>
-        <NavHamburger on:click={toggle} />
-      </div>
-      <NavUl {hidden}>
-        <NavLi href="/" active={true}>Home</NavLi>
-        <NavLi href="/about">About</NavLi>
-        <NavLi href="/services">Services</NavLi>
-        <NavLi href="/pricing">Pricing</NavLi>
-        <NavLi href="/contact">Contact</NavLi>
-      </NavUl>
-    </Navbar>
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            class="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span
+            class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+          >
+            Rental Acquisitions
+          </span>
+        </NavBrand>
+        <div class="flex md:order-2">
+          <Button size="sm" on:click={logOut}>Logout</Button>
+          <NavHamburger on:click={toggle} />
+        </div>
+        <NavUl {hidden}>
+          <NavLi
+            href="/"
+            on:click={(event) => {
+              event.preventDefault();
+              menu = 0;
+            }}
+            >Home
+          </NavLi>
+          <NavLi
+            href="/"
+            on:click={(event) => {
+              event.preventDefault();
+              menu = 1;
+            }}
+            >About
+          </NavLi>
+        </NavUl>
+      </Navbar>
+    </div>
+  {/if}
+  {#if menu === 0}
+    <div class="flex "><h1>Home</h1></div>
+  {:else if menu === 1}
+    <div class="flex "><h1>About</h1></div>
   {/if}
 </div>
